9871fee028a4bf1580704a3dd3b54d65caa01daa
Zsombor Németh <zsombor.nemeth99@gmail.com>
2020-11-12 11:34:33 +0100

    Pekseg osztály kész

---
+++ PeksegBeadando/Pekseg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeksegBeadando
+{
+    class Pekseg
+    {
+        public string Nev { get; set; }
+        public List<Pekaru> Termekek { get; }
+        public DateTime Alapitva { get; }
+
+        public override string ToString()
+        {
+            return $"{Nev} - {Alapitva.ToShortDateString()}";
+        }
+    }
+}
